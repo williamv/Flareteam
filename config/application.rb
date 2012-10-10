f6639b170414deb55daae72e30a03ea334da87c5
Cameron Cundiff <ckundo@gmail.com>
2012-10-09 21:03:49 -0400

    Add custom devise failure for unauthorized.
    [Fixes #37312395]

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Flareteam
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+    config.autoload_paths += %W(#{config.root}/lib/devise)
     config.assets.enabled = true
     config.assets.version = '1.0'
   end
