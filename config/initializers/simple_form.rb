9cd1bde03c1068ff0967538fa59b4903be0cf38d
Cameron Cundiff <ckundo@gmail.com>
2012-10-09 21:51:04 -0400

    Fix simple_form deprecation warning

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -7,7 +7,7 @@ SimpleForm.setup do |config|
   # whole input.
 
   config.wrappers :checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
-    b.use :tag => 'div', :class => 'controls' do |ba|
+    b.wrapper(:tag => 'div', :class => 'controls') do |ba|
       ba.use :label_input
     end
   end
