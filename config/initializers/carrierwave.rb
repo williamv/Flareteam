3440ece57b44d9c06fdb26dd42cbb76e56838b9b
Cameron Cundiff <ckundo@gmail.com>
2012-10-29 13:01:54 -0400

    Hotfix for epoch seconds

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,6 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = { provider: 'AWS' }
   config.fog_directory  = 'flareteam-comment-attachments'
-  config.fog_authenticated_url_expiration = 1667001600000
+  config.fog_authenticated_url_expiration = 432000000
 end
 
