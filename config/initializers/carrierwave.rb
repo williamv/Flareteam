dfd0720201385558c866b113f239b154b48ef61e
Cameron Cundiff <ckundo@gmail.com>
2012-10-29 12:56:46 -0400

    Hotfix for s3 expiration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,5 +7,6 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = { provider: 'AWS' }
   config.fog_directory  = 'flareteam-comment-attachments'
+  config.fog_authenticated_url_expiration = 1667001600000
 end
 
