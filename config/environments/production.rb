1a73618e1f3ac8b85a6a020d8da5b078ce517361
John Barker <jebarker@gmail.com>
2013-01-05 18:26:58 -0500

    Enforce SSL connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Flareteam::Application.configure do
   config.assets.digest = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
