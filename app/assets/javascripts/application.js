2868448491ef7a58d628c5f38d26e4e453fd6d20
John Barker <jebarker@gmail.com>
2013-04-14 11:35:20 -0400

    Extrat $.deparam from jquery bbq to fix dropdown bug
     - Looks like the minified version of bbq was possibly getting munged by
       the asset pipeline. It was failing in Firefox with an error 'f is
       undefined'. So replaced with the extracted deparam file instead.
    
    [Fixes #45462477]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ba-bbq.min
+//= require jquery-deparam
 //= require twitter/bootstrap
 //= require clientBehaviors
 //= require helpMailer
